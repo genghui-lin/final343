1c1946ad8ba8876925da2ff499a76c59f93de1ee
final343 <GENGHUI.LIN54@qmail.cuny.edu>
2025-05-19 22:56:59 -0400

    init commit

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-gray-800 mb-2">Final Exam 343</h1>
+      <h2 className="text-2xl font-semibold text-gray-700 mb-1">Genghui, Lin</h2>
+      <h3 className="text-xl text-gray-600">Spring 2025</h3>   
+    </>
+  )
+}
